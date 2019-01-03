38c386f0ebb316b9a2ea8b1d9a85eede79ea5f98
Emiliano Gallardo <eagallardo@fi.uba.ar>
2019-01-02 21:31:27 -0300

    Add bcryptjs

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -2,15 +2,14 @@
 
 const router = require('express').Router();
 const User = require('../models/user');
+const bcrypt = require('bcryptjs');
 
 router.post('/', function (req, res, next){
-  const params = {
-    username: req.body.username,
-    password: req.body.password
-  }
 
-  User.findOne(params).then(function(user){
-    if(!user) return res.status(401).send('Invalid credentials');
+  User.findOne({username: req.body.username}).then(function(user){
+    const valid = bcrypt.compareSync(req.body.password, user.password)
+    if (!user || !valid) return res.status(401).send('Invalid credentials');
+
     res.status(200).send({ status: 'OK', _id: user.id});
   }).catch(function(err) {
     res.status(400).send('Something went wrong. Error:', err);
--- src/controllers/register.js
+++ src/controllers/register.js
@@ -2,17 +2,18 @@
 
 const router = require('express').Router();
 const User = require('../models/user');
+const bcrypt = require('bcryptjs');
 
 router.post('/', function (req, res, next){
   const user = new User({
     first_name: req.body.first_name,
     last_name: req.body.last_name,
     username: req.body.username,
-    password: req.body.password,
-  })
+    password: bcrypt.hashSync(req.body.password, 10)
+  });
 
   user.save().then(function (){
-    res.status(200)
+    res.status(200).send({ status: 'OK', _id: user.id});
   }).catch(function (err) {
     res.status(400).send('Something went wrong. Error:', err)
   });
